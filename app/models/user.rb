8b04c0f1bebcbc2ef63beeb2998fd08d826ec78e
Dang <2d2l95@gmail.com>
2020-10-06 15:27:36 +0700

    minor_fix#4

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,6 @@ class User < ApplicationRecord
     has_one :product
   	validates_presence_of :name
   	validates_presence_of :phone
-  	validates_presence_of :address
   	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   	validates :email, presence: true, format: { with:VALID_EMAIL_REGEX },
     	uniqueness: {case_sensitive: false}
